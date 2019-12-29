24acc64073c7689053b6f1a143f2613d4d26eea4
tangenta <tangenta@126.com>
2019-12-29 22:48:14 +0800

    add sql_type.go

--- v4/export/writer_util.go
+++ v4/export/writer_util.go
@@ -1,7 +1,6 @@
 package export
 
 import (
-	"database/sql"
 	"fmt"
 	"io"
 	"strings"
@@ -50,7 +49,7 @@ func WriteInsert(tblIR TableDataIR, w io.StringWriter, cfg *Config) error {
 	}
 
 	for rowIter.HasNext() {
-		row := makeRowReceiver(tblIR.ColumnTypes())
+		row := MakeRowReceiver(tblIR.ColumnTypes())
 		if err := rowIter.Next(row); err != nil {
 			log.Error("scanning from sql.Row failed, error: %s", err.Error())
 			return err
@@ -84,133 +83,4 @@ func write(writer io.StringWriter, str string, logger Logger) error {
 
 func wrapStringWith(str string, wrapper string) string {
 	return fmt.Sprintf("%s%s%s", wrapper, str, wrapper)
-}
-
-func SQLTypeStringMaker() RowReceiverStringer {
-	return &SQLTypeString{}
-}
-
-func SQLTypeBytesMaker() RowReceiverStringer {
-	return &SQLTypeBytes{}
-}
-
-func SQLTypeNumberMaker() RowReceiverStringer {
-	return &SQLTypeInteger{}
-}
-
-var colTypeRowReceiverMap = map[string]func() RowReceiverStringer{
-	"CHAR":      SQLTypeStringMaker,
-	"NCHAR":     SQLTypeStringMaker,
-	"VARCHAR":   SQLTypeStringMaker,
-	"NVARCHAR":  SQLTypeStringMaker,
-	"TEXT":      SQLTypeStringMaker,
-	"ENUM":      SQLTypeStringMaker,
-	"SET":       SQLTypeStringMaker,
-	"JSON":      SQLTypeStringMaker,
-	"TIMESTAMP": SQLTypeStringMaker,
-	"DATETIME":  SQLTypeStringMaker,
-	"DATE":      SQLTypeStringMaker,
-	"TIME":      SQLTypeStringMaker,
-	"YEAR":      SQLTypeStringMaker,
-
-	"BINARY":    SQLTypeBytesMaker,
-	"VARBINARY": SQLTypeBytesMaker,
-	"BLOB":      SQLTypeBytesMaker,
-	"BIT":       SQLTypeBytesMaker,
-
-	"INT":     SQLTypeNumberMaker,
-	"FLOAT":   SQLTypeNumberMaker,
-	"DECIMAL": SQLTypeNumberMaker,
-}
-
-func makeRowReceiver(colTypes []string) RowReceiverStringer {
-	rowReceiverArr := make(RowReceiverArr, len(colTypes))
-	for i, colTp := range colTypes {
-		recMaker, ok := colTypeRowReceiverMap[colTp]
-		if !ok {
-			recMaker = SQLTypeStringMaker
-		}
-		rowReceiverArr[i] = recMaker()
-	}
-	return rowReceiverArr
-}
-
-type RowReceiverArr []RowReceiverStringer
-
-func (r RowReceiverArr) BindAddress(args []interface{}) {
-	for i := range args {
-		var singleAddr [1]interface{}
-		r[i].BindAddress(singleAddr[:])
-		args[i] = singleAddr[0]
-	}
-}
-func (r RowReceiverArr) ReportSize() uint64 {
-	var sum uint64
-	for _, receiver := range r {
-		sum += receiver.ReportSize()
-	}
-	return sum
-}
-func (r RowReceiverArr) ToString() string {
-	var sb strings.Builder
-	sb.WriteString("(")
-	for i, receiver := range r {
-		sb.WriteString(receiver.ToString())
-		if i != len(r)-1 {
-			sb.WriteString(", ")
-		}
-	}
-	sb.WriteString(")")
-	return sb.String()
-}
-
-type SQLTypeInteger struct {
-	SQLTypeString
-}
-
-func (s SQLTypeInteger) ToString() string {
-	if s.Valid {
-		return s.String
-	} else {
-		return "NULL"
-	}
-}
-
-type SQLTypeString struct {
-	sql.NullString
-}
-
-func (s *SQLTypeString) BindAddress(arg []interface{}) {
-	arg[0] = s
-}
-func (s *SQLTypeString) ReportSize() uint64 {
-	if s.Valid {
-		return uint64(len(s.String))
-	}
-	return uint64(len("NULL"))
-}
-func (s *SQLTypeString) ToString() string {
-	if s.Valid {
-		return fmt.Sprintf(`'%s'`, escapeSingleQuote(s.String))
-	} else {
-		return "NULL"
-	}
-}
-
-func escapeSingleQuote(src string) string {
-	return strings.ReplaceAll(src, "'", "\\'")
-}
-
-type SQLTypeBytes struct {
-	bytes []byte
-}
-
-func (s *SQLTypeBytes) BindAddress(arg []interface{}) {
-	arg[0] = &s.bytes
-}
-func (s *SQLTypeBytes) ReportSize() uint64 {
-	return uint64(len(s.bytes))
-}
-func (s *SQLTypeBytes) ToString() string {
-	return fmt.Sprintf("x'%x'", s.bytes)
 }
