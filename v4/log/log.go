2e68c8f3b56aa3a75cbd5a1f768b1b86aae89cce
tangenta <tangenta@126.com>
2020-01-08 22:25:28 +0800

    log: add a global zap.Logger

---
+++ v4/log/log.go
@@ -0,0 +1,28 @@
+package log
+
+import (
+	"log"
+
+	"go.uber.org/zap"
+)
+
+var globalLogger *zap.Logger
+
+func init() {
+	logger, err := zap.NewDevelopment(zap.AddCaller())
+	if err != nil {
+		log.Fatalf("unable to init zap.Logger: %s", err.Error())
+	}
+	globalLogger = logger
+	logger.Sugar()
+}
+
+// Zap returns the global dumpling logger.
+func Zap() *zap.Logger {
+	return globalLogger
+}
+
+// Sugar returns the sugared global dumpling logger.
+func Sugar() *zap.SugaredLogger {
+	return globalLogger.Sugar()
+}
