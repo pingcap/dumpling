6f654515251a5bd045fbdcd32dc98e694939278a
tangenta <tangenta@126.com>
2020-01-08 20:51:01 +0800

    tests: using environment variables to control DSN

--- tests/run.sh
+++ tests/run.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
+set -e
+
+DUMPLING_TEST_DIR=${DUMPLING_TEST_DIR:-"/tmp/dumpling_test_result"}
+DUMPLING_TEST_USER=${DUMPLING_TEST_USER:-"root"}
+DUMPLING_TEST_HOST=${DUMPLING_TEST_HOST:-"127.0.0.1"}
+DUMPLING_TEST_PORT=${DUMPLING_TEST_PORT:-"3306"}
+DUMPLING_TEST_PASSWORD=${DUMPLING_TEST_PASSWORD:-""}
+
+export DUMPLING_TEST_DIR
+export DUMPLING_TEST_USER
+export DUMPLING_TEST_HOST
+export DUMPLING_TEST_PORT
+export DUMPLING_TEST_PASSWORD
+
 set -eu
 
-export TEST_DIR=/tmp/dumpling_test_result
-export TEST_USER=root
-export TEST_HOST=127.0.0.1
-export TEST_PORT=3306
-export TEST_PASSWORD=""
-
-mkdir -p "$TEST_DIR"
+mkdir -p "$DUMPLING_TEST_DIR"
 PATH="tests/_utils:$PATH"
 
 test_connection() {
@@ -27,11 +35,11 @@ test_connection
 
 for script in tests/*/run.sh; do
     echo "Running test $script..."
-    BASE_NAME="$(dirname "$script")"
-    export BASE_NAME
+    DUMPLING_BASE_NAME="$(dirname "$script")"
+    export DUMPLING_BASE_NAME
     TEST_NAME="$(basename "$(dirname "$script")")"
-    OUTPUT_DIR="$TEST_DIR"/sql_res."$TEST_NAME"
-    export OUTPUT_DIR
+    DUMPLING_OUTPUT_DIR="$DUMPLING_TEST_DIR"/sql_res."$TEST_NAME"
+    export DUMPLING_OUTPUT_DIR
 
     PATH="tests/_utils:$PATH" \
     sh "$script"
