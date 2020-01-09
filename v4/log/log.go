f456e5d13963a1bbb9ec75a2745205773bb9b956
tangenta <tangenta@126.com>
2020-01-09 17:42:18 +0800

    log: wrap zap.logger with pingcap/log

--- v4/log/log.go
+++ v4/log/log.go
@@ -1,23 +1,59 @@
 package log
 
 import (
-	"log"
-
+	"github.com/pingcap/errors"
+	pclog "github.com/pingcap/log"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
-var globalLogger *zap.Logger
+var (
+	appLogger = Logger{zap.NewNop()}
+	appLevel  = zap.NewAtomicLevel()
+)
 
-func init() {
-	logger, err := zap.NewDevelopment(zap.AddCaller())
-	if err != nil {
-		log.Fatalf("unable to init zap.Logger: %s", err.Error())
-	}
-	globalLogger = logger
-	logger.Sugar()
+type Logger struct {
+	*zap.Logger
 }
 
-// Zap returns the global dumpling logger.
-func Zap() *zap.Logger {
-	return globalLogger
+// Zap returns the global logger.
+func Zap() Logger {
+	return appLogger
+}
+
+// Config serializes log related config in toml/json.
+type Config struct {
+	// Log level.
+	// One of "debug", "info", "warn", "error", "dpanic", "panic", and "fatal".
+	Level string `toml:"level" json:"level"`
+	// Log filename, leave empty to disable file log.
+	File string `toml:"file" json:"file"`
+	// Max size for a single file, in MB.
+	FileMaxSize int `toml:"max-size" json:"max-size"`
+	// Max log keep days, default is never deleting.
+	FileMaxDays int `toml:"max-days" json:"max-days"`
+	// Maximum number of old log files to retain.
+	FileMaxBackups int `toml:"max-backups" json:"max-backups"`
+}
+
+func InitAppLogger(cfg *Config) error {
+	logger, props, err := pclog.InitLogger(&pclog.Config{
+		Level: cfg.Level,
+		File: pclog.FileLogConfig{
+			Filename:   cfg.File,
+			MaxSize:    cfg.FileMaxSize,
+			MaxDays:    cfg.FileMaxDays,
+			MaxBackups: cfg.FileMaxBackups,
+		},
+	})
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	appLogger = Logger{logger}
+	appLevel = props.Level
+	return nil
+}
+
+func ChangeAppLogLevel(level zapcore.Level) {
+	appLevel.SetLevel(level)
 }
