4e9a16afcd40e402dbc0afd602d2149415e2980a
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2020-06-03 20:11:26 +0800

    change mv to cp

--- tests/quote/run.sh
+++ tests/quote/run.sh
@@ -3,9 +3,9 @@
 set -eu
 
 mkdir -p "$DUMPLING_OUTPUT_DIR"/data
-mv "$DUMPLING_BASE_NAME/data/quote-database.quote-table.0.sql" "$DUMPLING_OUTPUT_DIR/data/quo\`te-database.quo\`te-table.0.sql"
-mv "$DUMPLING_BASE_NAME/data/quote-database.quote-table-schema.sql" "$DUMPLING_OUTPUT_DIR/data/quo\`te-database.quo\`te-table-schema.sql"
-mv "$DUMPLING_BASE_NAME/data/quote-database-schema-create.sql" "$DUMPLING_OUTPUT_DIR/data/quo\`te-database-schema-create.sql"
+cp "$DUMPLING_BASE_NAME/data/quote-database.quote-table.0.sql" "$DUMPLING_OUTPUT_DIR/data/quo\`te-database.quo\`te-table.0.sql"
+cp "$DUMPLING_BASE_NAME/data/quote-database.quote-table-schema.sql" "$DUMPLING_OUTPUT_DIR/data/quo\`te-database.quo\`te-table-schema.sql"
+cp "$DUMPLING_BASE_NAME/data/quote-database-schema-create.sql" "$DUMPLING_OUTPUT_DIR/data/quo\`te-database-schema-create.sql"
 
 db="quo\`te-database"
 run_sql "drop database if exists \`quo\`\`te-database\`"
