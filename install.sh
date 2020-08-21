ddde090d4faa3b8ba9d193eaa09c9fe80fadf373
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2020-08-21 10:59:28 +0800

    address comments

--- install.sh
+++ install.sh
@@ -12,8 +12,9 @@ wget http://download.pingcap.org/tidb-toolkit-$TAG-linux-amd64.tar.gz -O tools.t
 tar -xzvf tools.tar.gz
 mv tidb-toolkit-$TAG-linux-amd64/bin/* bin/
 
+TIDB_TAG="v4.0.4"
 # download tidb-server
-git clone -b $TAG https://github.com/pingcap/tidb
+git clone -b $TIDB_TAG https://github.com/pingcap/tidb
 cd $pwd/tidb && make
 cd $pwd
 mv tidb/bin/tidb-server bin/
