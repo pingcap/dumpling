61c18d989a65f30056d7ac5a3b35ed5841721bf1
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2020-11-09 10:27:21 +0800

    tmp

--- v4/export/dump.go
+++ v4/export/dump.go
@@ -253,12 +253,12 @@ func dumpDatabases(pCtx context.Context, conf *Config, connectPool *connectionsP
 			for _, tableIR := range tableDataIRArray {
 				tableIR := tableIR
 				g.Go(func() error {
+					conn := connectPool.getConn()
+					defer connectPool.releaseConn(conn)
 					retryTime := 1
 					return utils.WithRetry(ctx, func() error {
 						log.Debug("trying to dump table chunk", zap.Int("retryTime", retryTime), zap.String("db", tableIR.DatabaseName()),
 							zap.String("table", tableIR.TableName()), zap.Int("chunkIndex", tableIR.ChunkIndex()))
-						conn := connectPool.getConn()
-						defer connectPool.releaseConn(conn)
 						retryTime += 1
 						err := tableIR.Start(ctx, conn)
 						if err != nil {
