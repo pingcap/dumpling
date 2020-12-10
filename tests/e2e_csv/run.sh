545559419310a3546dd2ec1816b32a3a4c13b18a
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2020-12-10 14:28:47 +0800

    add separator test

--- tests/e2e_csv/run.sh
+++ tests/e2e_csv/run.sh
@@ -19,10 +19,10 @@ run_sql_file "$DUMPLING_BASE_NAME/data/e2e_csv.t-schema.sql"
 run_sql_file "$DUMPLING_BASE_NAME/data/e2e_csv.t.sql"
 
 run() {
-
     echo "*** running subtest case ***"
     echo "escape_backslash is $escape_backslash"
     echo "csv_delimiter is $csv_delimiter"
+    echo "csv_separator is $csv_separator"
 
     # drop database on tidb
     export DUMPLING_TEST_PORT=4000
@@ -32,13 +32,14 @@ run() {
     # dumping
     export DUMPLING_TEST_PORT=3306
     export DUMPLING_TEST_DATABASE=$DB_NAME
-    run_dumpling --filetype="csv" --escape-backslash=$escape_backslash --csv-delimiter="$csv_delimiter"
+    run_dumpling --filetype="csv" --escape-backslash=$escape_backslash --csv-delimiter="$csv_delimiter" --csv-separator="$csv_separator"
 
     # construct lightning configuration
     mkdir -p $DUMPLING_TEST_DIR/conf
     cp "$cur/conf/lightning.toml" $DUMPLING_TEST_DIR/conf
 
     sed -i -e "s/delimiter-place-holder/$csv_delimiter/g" $DUMPLING_TEST_DIR/conf/lightning.toml
+    sed -i -e "s/separator-place-holder/$csv_separator/g" $DUMPLING_TEST_DIR/conf/lightning.toml
     escape_backslash_holder="true"
     if [[ $escape_backslash == "false" && $csv_delimiter != "" ]]; then
         escape_backslash_holder="false"
@@ -55,13 +56,17 @@ run() {
 
 escape_backslash_arr=("true" "false")
 csv_delimiter_arr=('"')
+csv_separator_arr=(',' 'a' 'aa' '|*|')
 
 for escape_backslash in ${escape_backslash_arr[@]}
 do
-  for csv_delimiter in ${csv_delimiter_arr[@]}
+  for csv_separator in ${csv_separator_arr[@]}
   do
+    for csv_delimiter in ${csv_delimiter_arr[@]}
+    do
+      run $*
+    done
+    csv_delimiter=""
     run $*
   done
-  csv_delimiter=""
-  run $*
 done
