06da265cd1fb384d5242694ca7d239a68b8a29f2
gmhdbjd <gmhdbjd@gmail.com>
2021-01-21 17:42:23 +0800

    add ut

--- v4/export/config_test.go
+++ v4/export/config_test.go
@@ -4,6 +4,7 @@ package export
 
 import (
 	"context"
+	"strings"
 
 	. "github.com/pingcap/check"
 )
@@ -17,4 +18,32 @@ func (s *testConfigSuite) TestCreateExternalStorage(c *C) {
 	loc, err := mockConfig.createExternalStorage(context.Background())
 	c.Assert(err, IsNil)
 	c.Assert(loc.URI(), Matches, "file:.*")
+}
+
+func (s *testConfigSuite) TestParseAndAdjust(c *C) {
+	cfg := DefaultConfig()
+	c.Assert(cfg.Host, Equals, "127.0.0.1")
+	c.Assert(cfg.Password, Equals, "")
+	c.Assert(cfg.Port, Equals, 4000)
+	c.Assert(cfg.User, Equals, "root")
+	c.Assert(cfg.FileSize, Equals, uint64(0))
+
+	extraArgs := "--password 123456 --port 1234"
+	cfg.Host = "255.255.255.255"
+	cfg.Port = 4321
+	cfg.FileSize = uint64(0x12345)
+
+	err := cfg.ParseAndAdjust(strings.Split(extraArgs, " "))
+	c.Assert(err, IsNil)
+
+	c.Assert(cfg.Host, Equals, "255.255.255.255") // config manually
+	c.Assert(cfg.Password, Equals, "123456")      // config by extraArgs
+	c.Assert(cfg.Port, Equals, 1234)              // config both, overwrite by extraArgs
+	c.Assert(cfg.User, Equals, "root")            // default value
+
+	c.Assert(cfg.FileSize, Equals, uint64(0x0)) // WARN: extra define flagset can only config by extra args
+	extraArgs = "--filesize 1M"                 // otherwise will be overwritten to default value
+	err = cfg.ParseAndAdjust(strings.Split(extraArgs, " "))
+	c.Assert(err, IsNil)
+	c.Assert(cfg.FileSize, Equals, uint64(0x100000))
 }
