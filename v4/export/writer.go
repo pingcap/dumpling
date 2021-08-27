4af393905583ced50b40e9948db8e352fecf8f85
Little-Wallace <bupt2013211450@gmail.com>
2021-08-27 16:40:01 +0800

    fix format
    Signed-off-by: Little-Wallace <bupt2013211450@gmail.com>

--- v4/export/writer.go
+++ v4/export/writer.go
@@ -11,9 +11,9 @@ import (
 
 	tcontext "github.com/pingcap/dumpling/v4/context"
 
+	"github.com/pingcap/errors"
 	"github.com/pingcap/tidb/br/pkg/storage"
 	"github.com/pingcap/tidb/br/pkg/utils"
-	"github.com/pingcap/errors"
 	"go.uber.org/zap"
 )
 
